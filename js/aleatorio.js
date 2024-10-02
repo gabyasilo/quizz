470e7ba59d8d111ad8c56d5446aefdff62afbe79
gabymendes <108007652+gabyasilo@users.noreply.github.com>
2024-10-02 14:59:50 -0300

    m

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["helena", "maria", "ana", "erick", "julia", "mario", "Gabriel"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
